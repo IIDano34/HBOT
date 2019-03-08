d4717dbfa74d6297a3a1c2d09a9ddd23a042b3b2
IIDano34 <45509559+IIDano34@users.noreply.github.com>
2019-03-08 11:20:10 +0200

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -349,7 +349,6 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Zinx Music`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
